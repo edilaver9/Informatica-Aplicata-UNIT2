aef02e940cbd98227ab999fede5d5d484430a8d1
edilaver9 <65861483+edilaver9@users.noreply.github.com>
2024-12-03 18:34:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+﻿document.getElementById("monitorizare").addEventListener("dblclick", function () {
+    const images = [
+        "screenshots/ram.png",
+        "screenshots/cpu.png",
+        "screenshots/disk.png"
+    ];
+    let index = 0;
+    const container = document.getElementById("imagini-monitorizare");
+    container.innerHTML = '';
+
+    const slideshow = setInterval(() => {
+        container.innerHTML = `<img src="${images[index]}" alt="Monitorizare ${index + 1}">`;
+        index = (index + 1) % images.length;
+    }, 3000);
+
+    setTimeout(() => clearInterval(slideshow), 15000);
+});
